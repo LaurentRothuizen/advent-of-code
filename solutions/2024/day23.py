39b834ae37a6cd47d951355086c96977ab358ee3
Laurent Rothuizen <l.rothuizen@student.tue.nl>
2024-12-24 00:38:01 +0100

    day 23 2024

--- solutions/2024/day23.py
+++ solutions/2024/day23.py
@@ -1,5 +1,8 @@
+from itertools import combinations
 import sys
 import os
+from networkx.algorithms import clique
+import networkx as nx
 
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 sys.path.insert(0, project_root)
@@ -7,13 +10,70 @@ sys.path.insert(0, project_root)
 
 from utils.input_parser import read_input
 from utils.submit import submit
+def create_graph(data):
+    V = []
+    E = {}
+    for line in data:
+        temp = line.strip('\n').split('-')
+        V.append(temp[0])
+        V.append(temp[1])
+        E[temp[0]] = E.get(temp[0], []) + [temp[1]]
+        E[temp[1]] = E.get(temp[1], []) + [temp[0]]
+    V_u = list(set(V))
+    G = (V_u,E)
+    return G
+
+def is_clique_with_t(c,E):
+    x,y,z = c
+    if x[0] != 't' and y[0] != 't' and z[0] != 't':
+        return False
+    if y in E[x] and z in E[x] and z in E[y]:
+        return True
+    return False
+
+def get_all_3_cliques_with_t(G):
+    (V,E) = G
+    combination = list(combinations(V, 3))
+    cliques = []
+    for c in combination:
+        if is_clique_with_t(c,E):
+            cliques.append(c)      
+    return len(cliques)
+
 def solve_part1(data):
-    # Solution for Part 1
-    pass
+    G = create_graph(data)
+    all_3_cliques = get_all_3_cliques_with_t(G)
+    return all_3_cliques
+
+def code_clique(max_clique):
+    sorted_strings = sorted(max_clique)
+    return ','.join(sorted_strings)
 
 def solve_part2(data):
-    # Solution for Part 2
-    pass
+    max_clique = create_graph_for_max_clique(data)
+    return code_clique(max_clique)
+
+def create_graph_for_max_clique(data):
+    V = []
+    E = []
+    for line in data:
+        temp = line.strip('\n').split('-')
+        V.append(temp[0])
+        V.append(temp[1])
+        E.append((temp[0], temp[1]))
+        E.append((temp[1], temp[0]))
+    V_u = list(set(V))
+    G = nx.Graph()
+    G.add_edges_from(E)
+    cliques = clique.find_cliques(G)
+    max_clique_size = 0
+    max_clique = None
+    for index, clq in enumerate(cliques):
+        print( f'Maximal Clique {index+1} ', clq)
+        if len(clq) > max_clique_size:
+            max_clique_size = len(clq)
+            max_clique = clq
+    return max_clique
 
 if __name__ == "__main__":
     year, day = 2024, 23
@@ -23,8 +83,8 @@ if __name__ == "__main__":
     test_data = read_input(year, day, file_name="test.txt")
     
     # Test cases (update with known solutions for the test input)
-    known_test_solution_part1 = None  # Replace with the known result for part 1
-    known_test_solution_part2 = None  # Replace with the known result for part 2
+    known_test_solution_part1 = 7  # Replace with the known result for part 1
+    known_test_solution_part2 = 'co,de,ka,ta'  # Replace with the known result for part 2
     
     # Verify test cases for Part 1
     print(f"Testing Part 1 for Day 23, Year 2024...")
